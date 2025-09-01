90ae43ca31cf3a4fa35e50de7c36d7c3a51cd590
Yhyun31 <younghyun1954@naver.com>
2025-09-01 23:14:54 +0900

    ✨feat: 컴포넌트, 화면 추가

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,9 @@
 import Button from "./Button";
 import ErrorMessage from "./ErrorMessage";
 import Input from "./Input";
-//import Review from "./Review";
+import Review from "./Review";
 import AlertModal from "./AlertModal";
 //import LoginModal from "./LoginModal";
+import { TabBar } from "./TabBar.jsx";
 
-export { Button, ErrorMessage, Input, AlertModal };
+export { Button, ErrorMessage, Input, AlertModal, Review, TabBar };
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -7,16 +7,38 @@ import theme from "../theme";
 import SignupDonePage from "./SignupDonePage";
 import Hing from "./Hing";
 import SignupPage from "./SignupPage";
+import MainPage from "./MainPage";
+import Search from "./Search";
+import ChatList from "./ChatList";
+import Notifications from "./Notifications";
+import MyPage from "./MyPage";
+import AllPosts from "./AllPosts";
+import CreatePost from "./CreatePost";
+import Profile from "./Profile";
+import { TabBar } from "../components";
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <Routes>
-        <Route path="/done" element={<SignupDonePage />} />
-        <Route path="/hing" element={<Hing />} />
-        <Route path="/" element={<SignupPage />} />
-        {/* 다른 라우트들을 여기에 추가하세요 */}
-      </Routes>
+      <div style={{ paddingBottom: "20px" }}>
+        <Routes>
+          <Route path="/main" element={<MainPage />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/chatls" element={<ChatList />} />
+          <Route path="/notifications" element={<Notifications />} />
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/allposts" element={<AllPosts />} />
+          <Route path="/createpost" element={<CreatePost />} />
+          <Route path="/profile" element={<Profile />} />
+
+          <Route path="/done" element={<SignupDonePage />} />
+          <Route path="/hing" element={<Hing />} />
+          <Route path="/" element={<SignupPage />} />
+
+          {/* 다른 라우트들을 여기에 추가하세요 */}
+        </Routes>
+      </div>
+      <TabBar />
     </ThemeProvider>
   );
 };
