42c5b9d1520ac9bf61527a48417ddfe240702cd3
Yhyun31 <younghyun1954@naver.com>
2025-09-05 17:41:00 +0900

    ✨feat: 폰트 설정 추가/ 게시글 상세페이지 화면 추가 /레이아웃 추가

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { ThemeProvider } from "styled-components";
+import { ThemeProvider, createGlobalStyle } from "styled-components";
 import theme from "../theme";
 import SignupDonePage from "./SignupDonePage";
 import Hing from "./Hing";
@@ -20,10 +20,41 @@ import { TabBar } from "../components";
 import SigninPage from "./SigninPage";
 import SigninWithEmailPage from "./SigninWithEmailPage";
 import FindPwPage from "./FindPwPage";
+import Layout from "../components/Layout";
+import PostDetail from "./PostDetail";
+import MyPostDetail from "./MyPostDetail";
+
+const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: "NanumSquare_acR";
+    src: url("/fonts/NanumSquare_acR.ttf") format("truetype");
+    font-weight: 400;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: "NanumSquare_acB";
+    src: url("/fonts/NanumSquare_acB.ttf") format("truetype");
+    font-weight: 700;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: "NanumSquare_acEB";
+    src: url("/fonts/NanumSquare_acEB.ttf") format("truetype");
+    font-weight: 800;
+    font-style: normal;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: "NanumSquare_acR", sans-serif;
+  }
+`;
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <div style={{ paddingBottom: "20px" }}>
         <Routes>
           <Route path="/main" element={<MainPage />} />
@@ -35,20 +66,19 @@ const App = () => {
           <Route path="/createpost" element={<CreatePost />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/editprofile" element={<EditProfile />} />
+          <Route path="/postdetail/:postId" element={<PostDetail />} />
+          <Route path="/mypostdetail/:postId" element={<MyPostDetail />} />
           <Route path="/done" element={<SignupDonePage />} />
           <Route path="/hing" element={<Hing />} />
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/" element={<SigninPage />} />
           <Route path="/signinwithemail" element={<SigninWithEmailPage />} />
           <Route path="/findpw" element={<FindPwPage />} />
-          
-          
-       
+
           {/* 다른 라우트들을 여기에 추가하세요 */}
         </Routes>
       </div>
-      <TabBar />
-     
+      <Layout />
     </ThemeProvider>
   );
 };
