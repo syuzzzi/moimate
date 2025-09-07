538ed794bef03b7627428be61a4a264c6b1f0a91
Yhyun31 <younghyun1954@naver.com>
2025-09-07 20:26:01 +0900

    bugfix: 탭바가 보여야 하는 페이지 설정으로 변경

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -7,17 +7,18 @@ import { TabBar } from "./TabBar";
 const Layout = () => {
   const location = useLocation();
 
-  // Define the paths where you do NOT want to show the TabBar
-  const hiddenPaths = [
-    "/",
-    "/signup",
-    "/signinwithemail",
-    "/findpw",
-    "/done",
-    "/hing",
+  // 탭바가 보여야 하는 경로들을 명시적으로 정의합니다.
+  const visiblePaths = [
+    "/main",
+    "/search",
+    "/chatls",
+    "/notifications",
+    "/mypage",
   ];
 
-  const isTabBarVisible = !hiddenPaths.includes(location.pathname);
+  // 현재 경로가 visiblePaths에 포함되어 있는지 확인합니다.
+  // 이 방식이 '안 보이는 경로'를 나열하는 것보다 훨씬 직관적이고 효율적입니다.
+  const isTabBarVisible = visiblePaths.includes(location.pathname);
 
   return <>{isTabBarVisible && <TabBar />}</>;
 };
